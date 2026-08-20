c4e1bc1057261b762a950de2468b896fc24c7087
Yousef <yousefftaalip@gmail.com>
2026-08-20 16:54:44 +0300

    refactor: cleanup package directories to com.bdaey.voluamup

--- app/src/main/java/com/bdaey/volumeup/model/Contact.kt
+++
@@ -1,9 +0,0 @@
-package com.bdaey.volumeup.model
-
-data class Contact(
-    val id: String,
-    val name: String,
-    val phoneNumber: String,
-    val emailAddress: String? = null,
-    val photoUri: String? = null
-)
